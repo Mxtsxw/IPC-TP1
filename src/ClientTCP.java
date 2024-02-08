975bb706cbcc93e5aa9a398e046c862d46c4e085
Matthieu <randriantsoamatthieu@gmail.com>
2024-02-09 00:10:58 +0100

    E4 : L’horloge parlante++ – TCP

--- src/ClientTCP.java
+++ src/ClientTCP.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientTCP {
@@ -8,18 +9,29 @@ public class ClientTCP {
         try {
             Socket socket = new Socket("127.0.0.1", 1234);
 
+            // Créer un flux de sortie pour envoyer des commandes au serveur
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
             // Créer un flux d'entrée pour lire les données envoyées par le serveur
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            // Lire et afficher la réponse du serveur
-            String serverResponse = in.readLine();
-            System.out.println("Réponse du serveur : " + serverResponse);
+            // Envoyer les commandes au serveur
+            out.println("DATE");
+            System.out.println("Réponse du serveur : " + in.readLine());
+
+            out.println("HOUR");
+            System.out.println("Réponse du serveur : " + in.readLine());
+
+            out.println("FULL");
+            System.out.println("Réponse du serveur : " + in.readLine());
+
+            out.println("CLOSE");
+            System.out.println("Connexion fermée.");
 
             // Fermer la connexion avec le serveur
             socket.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
--- src/ServeurTCP.java
+++ src/ServeurTCP.java
@@ -1,22 +1,46 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class ServeurTCP {
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(1234);
+            System.out.println("Serveur en attente de connexion...");
 
             while (true){
                 Socket clientSocket = serverSocket.accept();
+                System.out.println("Connexion établie avec " + clientSocket.getInetAddress());
+
+                // Créer un flux d'entrée pour lire les demandes du client
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String request = "";
+                try {
+                    request = in.readLine();
+                } catch (SocketException e) {
+                    clientSocket.close();
+                    System.out.println("Connexion interrompue");
+                }
+
+                // Si le client demande la fermeture de la connexion
+                if (request.equals("CLOSE")){
+                    clientSocket.close();
+                    break;
+                }
 
                 // Créer un flux de sortie vers le client
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
                 // Envoyer l'heure actuelle au client
-                out.println("Heure actuelle du serveur : " + LocalTime.now());
+                out.println("Heure actuelle du serveur : " + getResponse(request));
                 out.flush();
 
                 // Fermer la connexion avec le client
@@ -25,5 +49,14 @@ public class ServeurTCP {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static String getResponse(String request){
+        return switch (request) {
+            case "DATE" -> String.valueOf(LocalDate.now());
+            case "HOUR" -> String.valueOf(LocalTime.now().getHour());
+            case "FULL" -> String.valueOf(LocalDateTime.now());
+            default -> "Requête inconnue";
+        };
     }
 }
